a0082b9b62b3e17218b466cfb1447c6f53f3fd19
saman <samanthush@gmail.com>
2024-03-05 07:10:46 +0530

    inital commit

---
+++ js/components/App.js
@@ -0,0 +1,13 @@
+import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
+
+export default {
+    components: { Assignments, Panel },
+
+    template: `
+        <div class="grid gap-6">
+
+            <assignments></assignments>
+        </div>    
+    `,
+}
